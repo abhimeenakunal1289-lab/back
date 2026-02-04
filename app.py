07584e9351c31889f6a063bfd5640f79d985564e
abhimeenakunal1289-lab <abhimeenakunal1289@gmail.com>
2026-02-05 04:53:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -667,6 +667,6 @@ def get_positions():
         return jsonify({"success": False, "error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=false)
 
     
